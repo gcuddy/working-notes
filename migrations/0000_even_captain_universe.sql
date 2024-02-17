6c6e4981eea7cd5fbc621424ab631c320bf0363c
Gus <gus.cuddy@gmail.com>
2024-02-17 12:01:35 -0500

    reset migrations

--- migrations/0000_even_captain_universe.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `notes` (
-	`id` text PRIMARY KEY NOT NULL,
-	`title` text,
-	`content` text,
-	`created_at` integer,
-	`updated_at` integer
-);
--- migrations/0002_amazing_demogoblin.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE backlinks ADD `text` text;--> statement-breakpoint
-ALTER TABLE backlinks ADD `context` text;--> statement-breakpoint
-ALTER TABLE notes ADD `created_at` integer;--> statement-breakpoint
-ALTER TABLE notes ADD `updated_at` integer;
